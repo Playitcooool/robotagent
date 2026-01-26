134df27df1362d69bc2ce3efbe273d9ef3ebb7fd
Playitcool_ <129354153+Playitcooool@users.noreply.github.com>
2026-01-26 19:14:43 +0800

    v2.5.1 simulation agent tools load

--- agents/SimulationAgent.py
+++ agents/SimulationAgent.py
@@ -1,12 +1,18 @@
 from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
+import tools.SubAgentTool
 
 chatBot = ChatOpenAI(
     base_url="http://localhost:1234",
     model="qwen2.5.1-coder-7b-instruct",
     api_key="no_need",
 )
-agent = create_agent(
+simulation_agent_tools = []
+for func_name in tools.SubAgentTool.__all__:
+    function = getattr(tools.SubAgentTool, func_name)
+    simulation_agent_tools.append(function)
+
+simulation_agent = create_agent(
     model=chatBot,
-    tools=[],
+    tools=simulation_agent_tools,
 )
